712ad27dbc62b15fccc4a0fca187eeb2c848df14
xiaowu <czdoge@icloud.com>
2015-11-22 12:37:07 +0800

    first update

---
+++ lisp/init-gui-frames.el
@@ -0,0 +1,29 @@
+;;----------------------------------------------------------------------------
+;; Suppress GUI features
+;;----------------------------------------------------------------------------
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+
+(set-frame-width (selected-frame) 120)
+(set-frame-height (selected-frame) 40)
+
+;;----------------------------------------------------------------------------
+;; Show a marker in the left fringe for lines not in the buffer
+;;----------------------------------------------------------------------------
+(setq indicate-empty-lines t)
+
+;; NO tool bar or scroll bar
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (with-selected-frame frame
+              (unless window-system
+                (set-frame-parameter nil 'menu-bar-lines 0)))))
+
+(provide 'init-gui-frames)
